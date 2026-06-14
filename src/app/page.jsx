285a9cadb5c044130d6d80491230ca0bcfccdedb
ManiTeymouri <maniteymoori78@gmail.com>
2026-06-14 19:18:04 +0330

    page modified

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import thumbnail2 from "@/assets/images/thumbnail-2.jpg"
 
 export default function Home() {
   return (
-    <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 px-5 py-2 sm:mx-10">
+    <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5  px-5 py-2 sm:mx-10">
       <VideoCard thumb={thumbnail2}/>
       <VideoCard/>
       <VideoCard thumb={thumbnail2}/>

1178b3a3b84e664cc429e72e878e3bde601c067c
ManiTeymouri <maniteymoori78@gmail.com>
2026-06-14 19:17:46 +0330

    modified

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,26 @@
 import "@/assets/styles/global.css"
 import Header from "../components/Header.jsx";
-
+import Sidebar from "../components/Sidebar.jsx";
 
 export default function RootLayout({children}) {
     
   return (
     <html lang="en">
-      <body className="bg-[#0F0F0F]">
+      <body className="bg-[#0F0F0F] text-white">
 
         <Header/>
-        {children}
+
+        <div className="grid grid-cols-[1fr_7fr]">
+          
+          <div>
+            <Sidebar/>
+          </div>
+
+          <div>
+            {children}
+          </div>
+
+        </div>
         
       </body>
     </html>

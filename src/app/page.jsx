4e01d406249c1c5e976dadfbb80b51e4bd63d752
ManiTeymouri <maniteymoori78@gmail.com>
2026-06-15 01:55:47 +0330

    update VideoCard component and connect with video data

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,34 @@
-import Image from "next/image";
 import VideoCard from "../components/VideoCard";
 
-export default function Home() {
+async function getVideos() {
+  const res = await fetch("http://localhost:3000/api/videos", {
+    cache: "no-store",
+  });
+
+  const data = await res.json();
+  return data;
+}
+
+export default async function Home() {
+  const videos = await getVideos();
+
   return (
     <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5  px-5 py-2 sm:mx-10">
-      <VideoCard />
+      {videos.map((video) => {
+        return (
+          <VideoCard
+            key={video.id}
+            thumb={video.thumb}
+            profile={video.profile}
+            username={video.username}
+            totalView={video.totalView}
+            dateVideo={video.dateVideo}
+            title={video.title}
+            videoPreview={video.videoPreview}
+            videoDuration={video.videoDuration}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -19,11 +19,11 @@ export default function VideoCard({
   return (
     // videocard Component
 
-    <div className="flex flex-col rounded-xl gap-1 group justify-center max-w-xl transtion-all cursor-pointer duration-300 hover:bg-[#141E32] p-2">
+    <div className="flex flex-col rounded-xl gap-1 group justify-center max-w-xl transition-all cursor-pointer duration-300 hover:bg-[#141E32] p-2">
       <Link href={"#"}>
         <div className="overflow-hidden rounded-xl mb-2 relative">
           <div className="absolute transition-opacity duration-500 group-hover:opacity-0 bg-black/50 rounded-full mb-1 mr-1 flex place-items-center z-2 bottom-0 right-0 px-2">
-            <span id="videoDuration" className="text-xs  font-bold">
+            <span className="videoDuration text-xs  font-bold">
               {videoDuration}
             </span>
           </div>
@@ -36,7 +36,8 @@ export default function VideoCard({
                   src={thumb}
                   alt="thumbnail video"
                   height={2048}
-                  width={2048}></Image>
+                  width={2048}
+                ></Image>
               }
               loadingOverlay={
                 <div className="absolute inset-0 flex items-center justify-center bg-black/50">
@@ -64,18 +65,17 @@ export default function VideoCard({
             src={profile}
             alt="profile"
             width={50}
-            height={50}></Image>
+            height={50}
+          ></Image>
         </div>
 
         <div className=" min-w-0 line-clamp-2 flex-auto text-white">
           {title}
         </div>
-        <div className="">
-          <MoreHorizontal className="text-white hover:bg-[#0a0f1a] rounded-full size-7 transition-all duration-200" />
-        </div>
+        <MoreHorizontal className="text-white hover:bg-[#0a0f1a] rounded-full size-7 transition-all duration-200" />
       </div>
       <div className="flex h-full whitespace-nowrap overflow-hidden items-end gap-2">
-        <div className="text-[#5CA0B7] text-sm">{username}</div>
+        <div className="text-[#5CA0B7] text-sm truncate">{username}</div>
         <div className="text-[#5CA0B7] font-thin text-xs">{totalView} view</div>
 
         <div className="text-[#5CA0B7] font-thin text-xs">{dateVideo}</div>
